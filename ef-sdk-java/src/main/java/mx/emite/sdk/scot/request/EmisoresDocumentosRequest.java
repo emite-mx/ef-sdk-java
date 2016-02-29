67df9d0a49e9507d857c0f84a2fd6403b60cbbed
Enrique Sanchez de la Barquera <enrique@emitefacturacion.mx>
2016-02-29 12:11:53 -0600

    cambio catalogo de estados

--- ef-sdk-java/src/main/java/mx/emite/sdk/scot/request/EmisoresDocumentosRequest.java
+++ ef-sdk-java/src/main/java/mx/emite/sdk/scot/request/EmisoresDocumentosRequest.java
@@ -2,12 +2,11 @@ package mx.emite.sdk.scot.request;
 
 import javax.validation.constraints.NotNull;
 
-import lombok.Builder;
 import lombok.Data;
 import mx.emite.sdk.cfdi32.anotaciones.Rfc;
+import mx.emite.sdk.utils.Utilerias;
 
 @Data
-@Builder
 public class EmisoresDocumentosRequest  {
 
 	/**
@@ -41,6 +40,52 @@ public class EmisoresDocumentosRequest  {
 	@NotNull 
 	private String pdf;
 	
+	public static EmisoresDocumentosBuilder builder(){
+		return new EmisoresDocumentosBuilder();
+	}
 	
-	
+
+	public static class EmisoresDocumentosBuilder {
+	private String token;
+	private String rfc;
+	private Integer idTipoDocumento;
+	private String pdf;
+	public EmisoresDocumentosBuilder token(String token) {
+	this.token=token;
+	return this;
+	}
+	public EmisoresDocumentosBuilder rfc(String rfc) {
+	this.rfc=rfc;
+	return this;
+	}
+	public EmisoresDocumentosBuilder idTipoDocumento(Integer idTipoDocumento) {
+	this.idTipoDocumento=idTipoDocumento;
+	return this;
+	}
+	/**
+	 * @param pdf documento binario codificado en base 64
+	 * @return this builder
+	 */
+	public EmisoresDocumentosBuilder pdf(String pdf) {
+	this.pdf=pdf;
+	return this;
+	}
+	/**
+	 * @param pdf documento binario sin codificar
+	 * @return this builder
+	 */
+	public EmisoresDocumentosBuilder pdf(byte[] pdf) {
+		this.pdf=Utilerias.codifica64Binario(pdf);
+		return this;
+	}
+	public EmisoresDocumentosRequest build(){
+	return new EmisoresDocumentosRequest(this);
+	}
+	}
+	private EmisoresDocumentosRequest(EmisoresDocumentosBuilder builder){
+	this.token=builder.token;
+	this.rfc=builder.rfc;
+	this.idTipoDocumento=builder.idTipoDocumento;
+	this.pdf=builder.pdf;
+	}
 }
