6a5b0782e891ecb6dfd08aeb38284e170a48acdf
Enrique Sanchez de la Barquera <enrique@emitefacturacion.mx>
2017-05-15 16:23:53 -0500

    tasaocuota

--- ef-sdk-java/src/main/java/mx/emite/sdk/enums/sat/cfdi33/TasaOCuota33.java
+++ ef-sdk-java/src/main/java/mx/emite/sdk/enums/sat/cfdi33/TasaOCuota33.java
@@ -1,37 +1,47 @@
 package mx.emite.sdk.enums.sat.cfdi33;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Optional;
 
 import lombok.Getter;
 
 @Getter
 public enum TasaOCuota33 {
 
-	IVA0("0.000000",false,null,new BigDecimal("0.000000"),TiposImpuesto33.IVA,TiposFactor33.TASA,true,true),
-	IVA16("0.160000",false,null,new BigDecimal("0.160000"),TiposImpuesto33.IVA,TiposFactor33.TASA,true,true),
-	IVA16R("0.160000",true,new BigDecimal("0.000000"),new BigDecimal("0.160000"),TiposImpuesto33.IVA,TiposFactor33.TASA,false,true),
-	IEPS265("0.265000",false,null,new BigDecimal("0.265000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
-	IEPS300("0.300000",false,null,new BigDecimal("0.300000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
-	IEPS530("0.530000",false,null,new BigDecimal("0.530000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
-	IEPS500("0.500000",false,null,new BigDecimal("0.500000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
-	IEPS1600("1.600000",false,null,new BigDecimal("1.600000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
-	IEPS304("0.304000",false,null,new BigDecimal("0.304000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
-	IEPS350("0.350000",false,null,new BigDecimal("0.350000"),TiposImpuesto33.IEPS,TiposFactor33.CUOTA,true,true),
-	IEPS059("0.059100",false,null,new BigDecimal("0.059100"),TiposImpuesto33.IEPS,TiposFactor33.CUOTA,true,true),
-	IEPS3("3.000000",false,null,new BigDecimal("3.000000"),TiposImpuesto33.IEPS,TiposFactor33.CUOTA,true,true),
-	IEPS298("0.298800",false,null,new BigDecimal("0.298800"),TiposImpuesto33.IEPS,TiposFactor33.CUOTA,true,true),
+	IVA0(false,null,new BigDecimal("0.000000"),TiposImpuesto33.IVA,TiposFactor33.TASA,true,false),
+	IVA16(false,null,new BigDecimal("0.160000"),TiposImpuesto33.IVA,TiposFactor33.TASA,true,false),
+	IVA16R(true,new BigDecimal("0.000000"),new BigDecimal("0.160000"),TiposImpuesto33.IVA,TiposFactor33.TASA,false,true),
+	IEPS265(false,null,new BigDecimal("0.265000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS300(false,null,new BigDecimal("0.300000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS530(false,null,new BigDecimal("0.530000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS500(false,null,new BigDecimal("0.500000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS1600(false,null,new BigDecimal("1.600000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS304(false,null,new BigDecimal("0.304000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS350(false,null,new BigDecimal("0.250000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	
+	IEPS09(false,null,new BigDecimal("0.090000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS08(false,null,new BigDecimal("0.080000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS07(false,null,new BigDecimal("0.070000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS06(false,null,new BigDecimal("0.060000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,true),
+	IEPS03(false,null,new BigDecimal("0.030000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,false),
+	IEPS00(false,null,new BigDecimal("0.000000"),TiposImpuesto33.IEPS,TiposFactor33.TASA,true,false),
+	
+	IEPS4377(true,new BigDecimal("0.000000"),new BigDecimal("43.770000"),TiposImpuesto33.IEPS,TiposFactor33.CUOTA,true,true),
+	
+	IEPS298(true,new BigDecimal("0.000000"),new BigDecimal("0.350000"),TiposImpuesto33.ISR,TiposFactor33.TASA,false,true),
 	
 	;
 
-	private final String idSat;
+	//private final String idSat;
 	private final boolean esRango;
 	private final BigDecimal valorMinimo,valorMaximo;
 	private final TiposImpuesto33 impuesto;
 	private final TiposFactor33 factor;
 	private final boolean traslado,retencion;
 	
-	TasaOCuota33(String idSat,boolean esRango,BigDecimal minimo,BigDecimal maximo,TiposImpuesto33 tipo,TiposFactor33 factor,boolean traslado,boolean retencion){
-		this.idSat=idSat;
+	TasaOCuota33(boolean esRango,BigDecimal minimo,BigDecimal maximo,TiposImpuesto33 tipo,TiposFactor33 factor,boolean traslado,boolean retencion){
+		
 		this.esRango=esRango;
 		this.valorMinimo=minimo;
 		this.valorMaximo=maximo;
@@ -41,8 +51,23 @@ public enum TasaOCuota33 {
 		this.retencion=retencion;
 	}
 
-	public static TasaOCuota33 buscaTasa(BigDecimal tasaOCuota, boolean esTraslado) {
-		for(TasaOCuota33 tc:values()){
+	public static TasaOCuota33 buscaTasa(boolean esTraslado,final BigDecimal tasaOCuota,final TiposImpuesto33 tipo,final TiposFactor33 factor) {
+		final Optional<TasaOCuota33> tasas = Arrays.asList(values()).stream().filter(i->i.esTasa(esTraslado,tasaOCuota,tipo,factor)).findAny();
+		if(tasas.isPresent())
+			return tasas.get();
+		return null;
+		
+		/*for(TasaOCuota33 tc:values()){
+			if(tc.getValorMinimo()!=null){
+				if(tc.getValorMinimo().compareTo(tasaOCuota)>=0 && tc.getValorMaximo().compareTo(tasaOCuota)<=0 && tasaOCuota.scale()==6){
+					if(esTraslado&&tc.isTraslado())
+						return tc;
+					if(!esTraslado&&tc.isRetencion())
+						return tc;
+				}
+			}
+			
+			else
 			if(tc.getValorMaximo().compareTo(tasaOCuota)==0 && tasaOCuota.scale()==6){
 				if(esTraslado&&tc.isTraslado())
 					return tc;
@@ -50,6 +75,49 @@ public enum TasaOCuota33 {
 					return tc;
 			}
 		}
-		return null;
+		return null;*/
 	}
+
+	private boolean esTasa(boolean esTraslado, BigDecimal tasaOCuota, TiposImpuesto33 tipo, TiposFactor33 factor) {
+		if(tasaOCuota==null)
+			return false;
+		if(!this.impuesto.equals(tipo))
+			return false;
+		if(!this.factor.equals(factor))
+			return false;
+		if(esTraslado && !this.traslado)
+			return false;
+		if(!esTraslado && !this.retencion)
+			return false;
+		if(tasaOCuota.scale()!=6)
+			return false;
+		
+		if(this.valorMinimo==null){
+			//valor fijo
+			if(this.getValorMaximo().compareTo(tasaOCuota)!=0) 
+				return false;			
+		}
+		else{
+			return dentroRango(tasaOCuota,this.getValorMinimo(),this.getValorMaximo());
+		}
+		
+		return true;
+	}
+	
+	/*public static void main(String[] args){
+		System.out.println(dentroRango(new BigDecimal(-1),new BigDecimal("0.000000"),new BigDecimal("0.160000")));
+		System.out.println(dentroRango(new BigDecimal(0),new BigDecimal("0.000000"),new BigDecimal("0.160000")));
+		System.out.println(dentroRango(new BigDecimal("0.16"),new BigDecimal("0.000000"),new BigDecimal("0.160000")));
+		System.out.println(dentroRango(new BigDecimal("0.161"),new BigDecimal("0.000000"),new BigDecimal("0.160000")));
+		System.out.println(dentroRango(new BigDecimal("0.1"),new BigDecimal("0.000000"),new BigDecimal("0.160000")));
+	}*/
+	
+	private static boolean dentroRango(final BigDecimal valor,final BigDecimal minimo,final BigDecimal maximo){
+		if(valor.compareTo(minimo)<0)
+			return false;
+		if(valor.compareTo(maximo)>0)
+			return false;
+		return true;
+	}
+	
 }
